08758b9f3cd679f9fd9467ac394d408a9e2ba18c
Harshith Varma <harshithvarma2003@gmail.com>
2026-09-18 23:37:32 +0530

    Created customer table and inserted the details into it

---
+++ customer_details_inserted.sql
@@ -0,0 +1,16 @@
+use cust_order_management;
+insert into
+customers (first_name, last_name, email, phone, country_code, date_of_birth) 
+values
+('Harshith', 'varma', 'harshithvarma.e@gmail.com', '738679xxxx', '+91', '2004-05-24'),
+('Raaga', 'krishna', 'raagakrishna@gmail.com', '9876543211', '+91', '2001-09-01'),
+('vedha', 'krishna', 'vedhakrishna1@email.com', '9876543212', '+1', '2000-01-15'),
+('Adithya', 'varma', 'adithyavarma2006@gmail.com', '9876543213', '+91', '2006-02-14'),
+('radha', 'krishna', 'radhakrishna2@gmail.com', '9876543214', '+44', '2007-01-20'),
+('Lahari', 'kandula', 'laharikandula.2@gmail.com', '9876543215', '+1', '2008-07-15'),
+('harsha', 'vardhan', 'harsha.vardhan@gmail.com', '9876543216', '+1', '2011-03-22'),
+('vinnu', '.P', 'vinnu.p@gmail.com', '9876543217', '+91', '2009-12-10'),
+('harshith', 'varma', 'harshithvarma2003@gmail.com', '9876543218', '+1', '2004-05-24'),
+('kapeeshwar', 'v', 'kapeeshwar.v@gmail.com', '9876543219', '+27', '2007-05-27');
+
+select * from customers;
